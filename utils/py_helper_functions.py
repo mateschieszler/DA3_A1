31c8ccfd5ce1e70495f0bc07ce27efae6ec6f9a5
mateschieszler <schieszler_mate@student.ceu.edu>
2024-01-18 10:46:18 +0100

    Add helper functions

---
+++ utils/py_helper_functions.py
@@ -0,0 +1,126 @@
+####################################################
+# Import packages
+####################################################
+import numpy as np
+import copy
+import pandas as pd
+from plotnine import *
+
+
+####################################################
+# Define global vars
+####################################################
+color = ["#3a5e8cFF", "#10a53dFF", "#541352FF", "#ffcf20FF", "#2f9aa0FF"]
+
+
+####################################################
+# Define helper functions
+####################################################
+def seq(start, stop, by, round_n=3):
+    return [round(x, round_n) for x in list(np.arange(start, stop, by))]
+
+
+def skew(l, round_n=3):
+    return round((np.mean(l) - np.median(l)) / np.std(l), round_n)
+
+
+def lspline(series, knots):
+    def knot_ceil(vector, knot):
+        vector_copy = copy.deepcopy(vector)
+        vector_copy[vector_copy > knot] = knot
+        return vector_copy
+
+    if type(knots) != list:
+        knots = [knots]
+    design_matrix = None
+    vector = series.values
+
+    for i in range(len(knots)):
+        # print(i)
+        # print(vector)
+        if i == 0:
+            column = knot_ceil(vector, knots[i])
+        else:
+            column = knot_ceil(vector, knots[i] - knots[i - 1])
+        # print(column)
+        if i == 0:
+            design_matrix = column
+        else:
+            design_matrix = np.column_stack((design_matrix, column))
+        # print(design_matrix)
+        vector = vector - column
+    design_matrix = np.column_stack((design_matrix, vector))
+    # print(design_matrix)
+    return design_matrix
+
+def create_calibration_plot(
+    data,
+    # file_name,
+    prob_var,
+    actual_var,
+    y_lab="Actual event probability",
+    n_bins=10,
+    breaks=None,
+):
+    if breaks is None:
+        breaks = np.around(
+            np.linspace(0, (n_bins + 1) / 10, num=n_bins + 1, endpoint=False),
+            decimals=1,
+        ).tolist()
+
+    data["prob_bin"] = pd.cut(data[prob_var], breaks, right=True, include_lowest=True)
+
+    binned_data = (
+        data.groupby("prob_bin")
+        .agg(
+            mean_prob=(prob_var, "mean"),
+            mean_actual=(actual_var, "mean"),
+            n=(actual_var, "size"),
+        )
+        .reset_index()
+    )
+
+    plot = (
+        ggplot(binned_data, aes("mean_prob", "mean_actual"))
+        + geom_line(color=color[0], size=1, show_legend=True)
+        + geom_point(
+            color=color[0],
+            size=1,
+            alpha=0.7,
+            show_legend=False,
+            na_rm=True,
+        )
+        + geom_segment(
+            x=min(breaks),
+            xend=max(breaks),
+            y=min(breaks),
+            yend=max(breaks),
+            color=color[1],
+            size=0.5,
+        )
+        + theme_bw()
+        + labs(x="Predicted event probability", y=y_lab)
+        + coord_cartesian(xlim=(0, 1), ylim=(0, 1))
+        + expand_limits(x=0.01, y=0.01)
+        + scale_y_continuous(expand=(0.01, 0.01), breaks=(seq(0, 1.1, 0.1)))
+        + scale_x_continuous(expand=(0.01, 0.01), breaks=(seq(0, 1.1, 0.1)))
+    )
+
+    return plot
+
+
+def poly(x, degree=1):
+    """
+    Fit Polynomial
+
+    These are non orthogonal factors, but it may not matter if
+    we only need this for predictions (without interpreting the
+    coefficients) or visualisation.
+    """
+    d = {}
+    for i in range(degree+1):
+        if i == 1:
+            d['x'] = x
+        else:
+            d[f'x**{i}'] = np.power(x, i)
+    return pd.DataFrame(d)
